88b91d4d82c21ee5e4ba07bdfea71086ee80e7ae
hingjae <ljh23270@naver.com>
2024-10-03 18:10:58 +0900

    revision 조회

---
+++ src/test/java/com/honey/member_history_management/envers/repository/MemberRevisionRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.honey.member_history_management.envers.repository;
+
+import com.honey.member_history_management.member.domain.Member;
+import com.honey.member_history_management.member.domain.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@ActiveProfiles("test")
+@Transactional
+@SpringBootTest
+class MemberRevisionRepositoryTest {
+
+    @Autowired
+    private MemberRevisionRepository memberRevisionRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void findAll() {
+        Member member = Member.builder()
+                .id("user1")
+                .password("pw1")
+                .name("foo")
+                .age(20)
+                .phoneNumber("010-1234-1234")
+                .build();
+
+        memberRepository.save(member);
+
+        List<Member> all = memberRevisionRepository.findAll();
+
+        System.out.println(all);
+    }
+}
