aae2a15d9efc93352a5e6745af439acd01da9b4e
hingjae <ljh23270@naver.com>
2024-10-03 23:31:42 +0900

    member modify form, delete event

--- src/main/java/com/honey/member_history_management/member/api/MemberService.java
+++ src/main/java/com/honey/member_history_management/member/api/MemberService.java
@@ -3,16 +3,17 @@ package com.honey.member_history_management.member.api;
 import com.honey.member_history_management.member.domain.Member;
 import com.honey.member_history_management.member.domain.MemberRepository;
 import com.honey.member_history_management.member.domain.Role;
-import com.honey.member_history_management.member.dto.MemberCreateForm;
+import com.honey.member_history_management.member.dto.MemberForm;
 import com.honey.member_history_management.team.api.TeamService;
 import com.honey.member_history_management.team.domain.Team;
-import com.honey.member_history_management.team.domain.TeamRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,10 +31,26 @@ public class MemberService {
     }
 
     @Transactional
-    public String create(MemberCreateForm form) {
+    public String create(MemberForm form) {
         Team team = teamService.findById(form.getTeamId());
         String encodedPassword = passwordEncoder.encode(form.getPassword());
         return memberRepository.save(form.toEntity(encodedPassword, Role.USER, team))
                 .getId();
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    @Transactional
+    public void modify(String memberId, MemberForm form) {
+        Member member = findById(memberId);
+        Team team = teamService.findById(form.getTeamId());
+        member.modify(team, form.getName(), form.getAge(), form.getPhoneNumber());
+    }
+
+    @Transactional
+    public void deleteById(String memberId) {
+        memberRepository.deleteById(memberId);
     }
 }
--- src/main/java/com/honey/member_history_management/member/dto/MemberForm.java
+++ src/main/java/com/honey/member_history_management/member/dto/MemberForm.java
@@ -7,27 +7,40 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class MemberCreateForm {
-    private String id;
+public class MemberForm {
+    private final String id;
 
-    private String password;
+    private final String password;
 
-    private String name;
+    private final String name;
 
-    private Integer age;
+    private final Integer age;
 
-    private String phoneNumber;
+    private final String phoneNumber;
 
-    private String teamId;
+    private final String teamId;
 
     @Builder
-    public MemberCreateForm(String id, String password, String name, Integer age, String phoneNumber, String teamId) {
+    public MemberForm(String id, String password, String name, Integer age, String phoneNumber, String teamId) {
         this.id = id;
         this.password = password;
         this.name = name;
         this.age = age;
         this.phoneNumber = phoneNumber;
         this.teamId = teamId;
+    }
+
+    public static MemberForm from(Member member) {
+        String teamId = member.getTeam() != null ? member.getTeam().getId() : null;
+
+        return MemberForm.builder()
+                .id(member.getId())
+                .password(member.getPassword())
+                .name(member.getName())
+                .age(member.getAge())
+                .phoneNumber(member.getPhoneNumber())
+                .teamId(teamId)
+                .build();
     }
 
     public Member toEntity(String encodedPassword, Role role, Team team) {
