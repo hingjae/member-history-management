3ee781d0ebde4a1904acd2480837669ca3603245
hingjae <ljh23270@naver.com>
2024-10-03 18:11:12 +0900

    삭제

--- docker/local/db/init.d/dml.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO member (id, name, age, phone_number, role, password, created_at, updated_at, created_by, updated_by)
-VALUES
-('honey', 'Alice', 30, '010-1234-5678', 'ADMIN', '$2a$10$VbG7eTYPHV6gJYXb4/OV/Oe8M2JuDZQyd7jOCXh.0ZT3H5pl7OXiG', NOW(), NOW(), 'system', 'system');
