5d69bab1add5912cb790bf8fb9439091bd64e902
hingjae <ljh23270@naver.com>
2024-10-05 20:02:03 +0900

    RevisionRepository 인터페이스

--- src/main/java/com/honey/member_history_management/MemberHistoryManagementApplication.java
+++ src/main/java/com/honey/member_history_management/MemberHistoryManagementApplication.java
@@ -2,7 +2,11 @@ package com.honey.member_history_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 @SpringBootApplication
 public class MemberHistoryManagementApplication {
 
--- src/main/java/com/honey/member_history_management/member/domain/MemberRepository.java
+++ src/main/java/com/honey/member_history_management/member/domain/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.honey.member_history_management.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.history.RevisionRepository;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+public interface MemberRepository extends JpaRepository<Member, String>, RevisionRepository<Member, String, Long> {
 }
